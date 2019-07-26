0f188abfab5c52ae77a72b13906421511d848884
Paul Laffitte <paul.laffitte@epitech.eu>
2019-07-26 19:41:39 +0200

    add yaml support

--- src/prebuild.js
+++ src/prebuild.js
@@ -5,19 +5,34 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp').sync;
 const mustache = require('mustache');
+const yaml = require('yaml-js');
+
+function loadSettings(parsedPath) {
+	const fullpath = `${parsedPath.dir}/${parsedPath.name}${parsedPath.ext}`
+
+	switch (parsedPath.ext) {
+		case '.json':
+			return require(fullpath);
+		case '.yml':
+		case '.yaml':
+			return yaml.load(fs.readFileSync(fullpath));
+		default:
+			throw new Error(`Extension "${parsedPath.ext}" is not supported`)
+	}
+}
 
 module.exports = async filename => {
 	const settingsPath = path.isAbsolute(filename) ? filename : path.resolve(filename);
-	const { revisions, prebuild: prebuildPath, template: templatePath, metadata } = require(settingsPath);
+	const parsedSettingsPath = path.parse(settingsPath);
+	const { revisions, prebuild: prebuildPath, template: templatePath, metadata } = loadSettings(parsedSettingsPath);
 	const documentFolder = path.dirname(settingsPath);
 	const prebuild = prebuildPath ? require(`${documentFolder}/${prebuildPath}`) : () => {};
 
 	const metadataArray = Object.keys(metadata).map(key => [ key, metadata[key] ]);
 	const metadataTable = table([ ["", ""], ...metadataArray ]);
-	const revisionsArray = revisions.map(({ date, version, authors, sections, comments }) => [ date, version, authors.join(), sections.join(), comments ]);
+	const revisionsArray = revisions.map(({ date, version, authors, sections, comments }) => [ date, version, authors ? authors.join() : '', sections ? sections.join() : '', comments ]);
 	const revisionsTable = table([ ["", ""], ...revisionsArray ]);
 
-	const parsedSettingsPath = path.parse(settingsPath);
 
 	mustache.escape = text => text;
 	metadata.folder = documentFolder;
