06702da6ad3fd01f503377a7177e6090f4e2e86f
Paul Laffitte <paul.laffitte@epitech.eu>
2019-11-08 23:28:55 +0700

    deliverable cards

--- data/PLD.js
+++ data/PLD.js
@@ -5,7 +5,7 @@ const config = require('./config');
 
 /*
 ISSUE EXAMPLE:
-le livrable - la catégorie
+le livrable - la catégorie${Math.floor(Math.random() * 5)}
 En tant que quelqu'un d'important, je veux faire quelque chose.
 
 C'est important pour une personne importante de pouvoir faire quelque chose.
@@ -35,7 +35,7 @@ module.exports = async (metadata, revisions) => {
 
 	const projectNamesByIds = {};
 	const issues = (await api.Issues.all({ milestone: milestone.title, scope: 'all' }));
-	const cards = (await Promise.all(issues.map(async ({ title, description, time_stats, project_id, labels, closed_at }) => {
+	const stories = (await Promise.all(issues.map(async ({ title, description, time_stats, project_id, labels, closed_at }) => {
 		const timeEstimate = time_stats.time_estimate / (8 * 3600);
 
 		if (!description || description.length == 0)
@@ -49,7 +49,7 @@ module.exports = async (metadata, revisions) => {
 		const [ category, deliverable ] = description.shift().split(' - ').reverse();
 		const [ story, user, goal ] = description.shift().match(/En tant que ([^,]*), je veux (.*)/) || [];
 
-		if (!user || !story)
+		if (!user || !goal)
 			return;
 
 		description = description.join('\n');
@@ -75,9 +75,60 @@ module.exports = async (metadata, revisions) => {
 		};
 	}))).filter(Boolean);
 
+	let deliverables = {};
+	let categories = [];
+
+	stories.forEach(({ deliverable, category }) => {
+		deliverables[deliverable] = {};
+		categories.push({
+			name: category,
+			deliverable
+		});
+	});
+
+	categories = [ ...new Set(categories) ].sort();
+	categories.forEach(category => deliverables[category.deliverable][category.name] = []);
+	deliverables.list = Object.keys(deliverables).sort();
+	deliverables.list.forEach(deliverable => deliverables[deliverable].categories = Object.keys(deliverables[deliverable]).sort());
+	stories.forEach(card => deliverables[card.deliverable][card.category].push(card));
+
+	deliverables = deliverables.list.map((deliverable, i) => {
+		return {
+			name: deliverable,
+			index: i + 1,
+			categories: deliverables[deliverable].categories.map((category, i) => {
+				return {
+					name: category,
+					index: i + 1,
+					stories: deliverables[deliverable][category].sort().map((story, i) => ({ ...story, index: i + 1 }))
+				};
+			})
+		};
+	});
+
+	const deliverableCards = deliverables.map(({ name, index, categories }) => {
+		const rows = [];
+		for (let i = 1; rows.length == 0 || rows[rows.length - 1].filter(({ story }) => story).length; i++) {
+			rows.push(categories.map(({ name, index, stories }) => ({
+				index,
+				story: (stories[stories.length - i] || {}).title
+			})));
+		}
+		rows.pop();
+		rows.reverse();
+
+		return {
+			name,
+			index,
+			headCells: categories.map(({ name, index }) => ({ name, index })),
+			rows,
+		}
+	});
+
 	return {
 		advancement_reports: fs.readFileSync(`${folder}/advancement_reports.md`).toString(),
 		date: new Date(),
-		cards,
+		deliverables,
+		deliverableCards
 	};
 };
--- data/PLD.md
+++ data/PLD.md
@@ -8,11 +8,41 @@
 {{tables.revisions}}
 
 ## Organigramme des livrables
+{{#data.deliverables}}
+### {{name}}
+{{/data.deliverables}}
 
 ## Cartes des livrables
+{{#data.deliverableCards}}
+<table>
+	<thead>
+		<tr>
+			<th colspan="{{headCells.length}}">{{name}}</th>
+		</tr>
+		<tr>
+		{{#headCells}}
+			<th>{{name}}</th>
+		{{/headCells}}
+		</tr>
+	</thead>
+	<tbody>
+	{{#rows}}
+		<tr>
+		{{#.}}
+			<td>{{story}}</td>
+		{{/.}}
+		<tr>
+	{{/rows}}
+	</tbody>
+</table>
+{{/data.deliverableCards}}
 
 ## Stories
-{{#data.cards}}
+{{#data.deliverables}}
+### {{name}}
+	{{#categories}}
+#### {{name}}
+		{{#stories}}
 <table class="{{status}}">
 	<thead>
 		<tr>
@@ -37,7 +67,9 @@
 		</tr>
 	</tbody>
 </table>
-{{/data.cards}}
+		{{/stories}}
+	{{/categories}}
+{{/data.deliverables}}
 
 ## Rapports d'avancement
 {{data.advancement_reports}}
