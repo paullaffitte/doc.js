b7648df1adb02a4746d21f90b11855af1006c7cb
Paul Laffitte <paul.laffitte@epitech.eu>
2019-11-08 20:33:33 +0700

    remove markdown output support

--- src/build.js
+++ src/build.js
@@ -7,7 +7,7 @@ const path = require('path');
 const prebuild = require('./prebuild');
 
 function parseFormat(value) {
-	const allowedFormats = [ 'pdf', 'html', 'md' ];
+	const allowedFormats = [ 'pdf', 'html' ];
 
 	if (!allowedFormats.includes(value)) {
 		throw new Error(`Invalid format "${value}", should be one of ${allowedFormats.join(', ')}.`);
@@ -20,13 +20,6 @@ commander
 	.option('-f, --format <extension>', 'output format', parseFormat)
 	.option('-o, --output <filename>', 'output filename')
 	.parse(process.argv);
-
-async function buildMarkdown(documentName) {
-	const files = await Promise.all(inputs.map(filename => fs.readFile(filename)));
-	const content = files.reduce((acc, file) => acc + file.toString(), '');
-
-	fs.writeFile(`${documentName}.md`, new Uint8Array(...files));
-}
 
 if (commander.args.length < 1)
 	throw new Error('Missing filename');
@@ -41,19 +34,15 @@ if (output.name && output.ext.length == 0)
 	commander.output += '.' + commander.format;
 
 prebuild(filename, output.name || documentName).then(documentFolder => {
-	if (commander.format == 'md') {
-		buildMarkdown(documentName);
-	} else {
-		execa('pandoc', [
-			'build/prebuild.md',
-			'-o', commander.output || `./build/${documentName}.${commander.format}`,
-			'--from', 'markdown-markdown_in_html_blocks+raw_html+auto_identifiers+header_attributes',
-			'-t', 'html5',
-			'--css', `${documentFolder}/${documentName}.css`,
-		])
-		.then(() => fs.unlink('build/prebuild.md'))
-		.catch(err => {
-			console.error(err);
-		});
-	}
+	execa('pandoc', [
+		'build/prebuild.md',
+		'-o', commander.output || `./build/${documentName}.${commander.format}`,
+		'--from', 'markdown-markdown_in_html_blocks+raw_html+auto_identifiers+header_attributes',
+		'-t', 'html5',
+		'--css', `${documentFolder}/${documentName}.css`,
+	])
+	.then(() => fs.unlink('build/prebuild.md'))
+	.catch(err => {
+		console.error(err);
+	});
 });
