d7c9000030af32c58f2e17989160b25202b1adf0
Paul Laffitte <paul.laffitte@epitech.eu>
2019-11-08 20:32:32 +0700

    snapshot mustache view and add output option

--- src/build.js
+++ src/build.js
@@ -18,6 +18,7 @@ function parseFormat(value) {
 
 commander
 	.option('-f, --format <extension>', 'output format', parseFormat)
+	.option('-o, --output <filename>', 'output filename')
 	.parse(process.argv);
 
 async function buildMarkdown(documentName) {
@@ -34,14 +35,18 @@ if (!commander.format)
 
 const filename = commander.args[0];
 const documentName = path.parse(filename).name;
+const output = commander.output ? path.parse(commander.output) : {};
 
-prebuild(filename).then(documentFolder => {
+if (output.name && output.ext.length == 0)
+	commander.output += '.' + commander.format;
+
+prebuild(filename, output.name || documentName).then(documentFolder => {
 	if (commander.format == 'md') {
 		buildMarkdown(documentName);
 	} else {
 		execa('pandoc', [
 			'build/prebuild.md',
-			'-o', `./build/${documentName}.${commander.format}`,
+			'-o', commander.output || `./build/${documentName}.${commander.format}`,
 			'--from', 'markdown-markdown_in_html_blocks+raw_html+auto_identifiers+header_attributes',
 			'-t', 'html5',
 			'--css', `${documentFolder}/${documentName}.css`,
