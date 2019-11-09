9ea35760097c4bbc3d9c28bdbbe5eb8d3c0d4aaf
Paul Laffitte <paul.laffitte@epitech.eu>
2019-11-09 22:20:51 +0700

    use puppeteer to render pdf and add header and footer template support

--- src/build.js
+++ src/build.js
@@ -2,9 +2,11 @@
 
 const execa = require('execa');
 const commander = require('commander');
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const RenderPDF = require('chrome-headless-render-pdf');
+const puppeteer = require('puppeteer');
+const image2base64 = require('image-to-base64');
 const prebuild = require('./prebuild');
 
 function parseFormat(value) {
@@ -15,6 +17,43 @@ function parseFormat(value) {
 	}
 
 	return value;
+}
+
+async function incorporateImagesAndStyle(html, css, margin) {
+	const matches = html.matchAll(/<img[^>]*? src="([^"]*)"/g);
+  const style = `font-size: 8pt; margin: 0 ${margin.right} 0 ${margin.left};`;
+
+	for (const match of matches) {
+		const imgTagWithBase64 = match[0].replace(match[1], `data:image/${match[1].split('.').pop()};base64,` + (await image2base64(match[1])));
+		html = html.replace(match[0], imgTagWithBase64);
+	}
+
+	return `<style>${css || ''}</style><div style="${style}">${html}</div>`
+}
+
+async function html2Pdf(inputLocation, outputFilename, options) {
+	const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const { header, footer, margin } = options;
+  const css = fs.existsSync(options.cssFilename) ? fs.readFileSync(options.cssFilename).toString() : '';
+
+  await page.goto(inputLocation, { waitUntil: 'networkidle2' });
+  await page.pdf({
+  	path: outputFilename,
+  	format: 'A4',
+  	displayHeaderFooter: true,
+  	printBackground: true,
+  	headerTemplate: await incorporateImagesAndStyle(header, css, margin),
+  	footerTemplate: await incorporateImagesAndStyle(footer, css, margin),
+  	margin: {
+  		top: margin.top,
+  		bottom: margin.bottom,
+  		left: margin.left,
+  		right: margin.right,
+  	}
+  });
+
+  await browser.close();
 }
 
 // TODO check pandoc version >=2.7.x
@@ -37,7 +76,8 @@ const parsedOutput = path.parse(output);
 if (output.name && output.ext.length == 0)
 	commander.output += '.html';
 
-prebuild(filename, output.name || documentName).then(documentFolder => {
+prebuild(filename, output.name || documentName).then((options) => {
+	const cssFilename = `${options.documentFolder}/${documentName}.css`;
 	const command = [
 		'build/prebuild.md',
 		'-o', output,
@@ -45,19 +85,19 @@ prebuild(filename, output.name || documentName).then(documentFolder => {
 		'-F', 'mermaid-filter',
 		'-t', 'html5',
 		'--metadata', `pagetitle=${documentName}`,
-		'--css', `${documentFolder}/${documentName}.css`,
+		'--css', cssFilename,
 		'--standalone'
 	];
 
 	console.log('pandoc', command.join(' '));
 	execa('pandoc', command)
 	.then(() => {
-		fs.unlink('build/prebuild.md');
+		fs.unlinkSync('build/prebuild.md');
 		if (commander.format == 'pdf') {
 			const htmlPath = path.resolve(`${__dirname}/../${output}`);
-			const pdfOutput = [ parsedOutput.dir, '/', parsedOutput.name, '.pdf' ].join('');
-			console.log('Generating pdf with google-chrome', pdfOutput);
-			RenderPDF.generateSinglePdf(`file://${htmlPath}`, pdfOutput, { includeBackground: true });
+			const outputFilename = [ parsedOutput.dir, '/', parsedOutput.name, '.pdf' ].join('');
+			console.log('Generating pdf with puppeteer', outputFilename);
+			html2Pdf(`file://${htmlPath}`, outputFilename, { ...options, cssFilename });
 		}
 	})
 	.catch(err => console.error(err));
