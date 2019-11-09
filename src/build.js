835478fff27df5fe91571cb4dbed3772bdf8e3b1
Paul Laffitte <paul.laffitte@epitech.eu>
2019-11-09 16:23:12 +0700

    add deliverables flow-chart, header and footer

--- src/build.js
+++ src/build.js
@@ -4,6 +4,7 @@ const execa = require('execa');
 const commander = require('commander');
 const fs = require('fs').promises;
 const path = require('path');
+const RenderPDF = require('chrome-headless-render-pdf');
 const prebuild = require('./prebuild');
 
 function parseFormat(value) {
@@ -15,6 +16,8 @@ function parseFormat(value) {
 
 	return value;
 }
+
+// TODO check pandoc version >=2.7.x
 
 commander
 	.option('-f, --format <extension>', 'output format', parseFormat)
@@ -28,28 +31,34 @@ if (!commander.format)
 
 const filename = commander.args[0];
 const documentName = path.parse(filename).name;
-const output = commander.output ? path.parse(commander.output) : {};
+const output = commander.output || `./build/${documentName}.html`;
+const parsedOutput = path.parse(output);
 
 if (output.name && output.ext.length == 0)
-	commander.output += '.' + commander.format;
+	commander.output += '.html';
 
 prebuild(filename, output.name || documentName).then(documentFolder => {
 	const command = [
 		'build/prebuild.md',
-		'-o', commander.output || `./build/${documentName}.${commander.format}`,
+		'-o', output,
 		'--from', 'markdown-markdown_in_html_blocks+raw_html+auto_identifiers+header_attributes',
+		'-F', 'mermaid-filter',
 		'-t', 'html5',
+		'--metadata', `pagetitle=${documentName}`,
 		'--css', `${documentFolder}/${documentName}.css`,
+		'--standalone'
 	];
 
 	console.log('pandoc', command.join(' '));
-
 	execa('pandoc', command)
 	.then(() => {
-		console.log('pandoc done');
-		fs.unlink('build/prebuild.md')
+		fs.unlink('build/prebuild.md');
+		if (commander.format == 'pdf') {
+			const htmlPath = path.resolve(`${__dirname}/../${output}`);
+			const pdfOutput = [ parsedOutput.dir, '/', parsedOutput.name, '.pdf' ].join('');
+			console.log('Generating pdf with google-chrome', pdfOutput);
+			RenderPDF.generateSinglePdf(`file://${htmlPath}`, pdfOutput, { includeBackground: true });
+		}
 	})
-	.catch(err => {
-		console.error(err);
-	});
+	.catch(err => console.error(err));
 });
--- src/prebuild.js
+++ src/prebuild.js
@@ -51,7 +51,7 @@ module.exports = async (filename, outputName) => {
 	view.tables = {
 		metadata: metadataArray,
 		revisions: revisionsArray,
-	}
+	};
 	fs.writeFileSync(`./build/${outputName}.json`, JSON.stringify(view, null, '\t'));
 
 	return documentFolder;
